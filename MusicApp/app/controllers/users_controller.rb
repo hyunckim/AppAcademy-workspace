957e714625808d7cb861e7ee2e4b1edd7db50ada
Hyunchul Kim <MasterHames@Hyunchuls-MacBook-Pro.local>
2017-02-16 10:48:28 -0800

    Create User Controller

--- MusicApp/app/controllers/users_controller.rb
+++ MusicApp/app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  # before_action :require_no_user!
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(users_param)
+    if @user.save!
+      #login user after signing in login_user!(@user)
+      redirect_to
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+    def users_param
+      params.require(:users).permit(:email, :password)
+    end
 end

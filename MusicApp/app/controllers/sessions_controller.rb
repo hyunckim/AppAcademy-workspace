38847abafc6a4cd0ed111ac36a4039f5d10975e9
Hyunchul Kim <MasterHames@Hyunchuls-MacBook-Pro.local>
2017-02-16 12:09:38 -0800

    Create Session Controller

---
+++ MusicApp/app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    if user.nil?
+      flash.now[:errors] = ["Invald email and/or password"]
+      render :new
+    else
+      login_user!(user)
+      redirect_to user_url(user)
+    end
+  end
+end
--- MusicApp/app/controllers/users_controller.rb
+++ MusicApp/app/controllers/users_controller.rb
@@ -1,23 +1,27 @@
 class UsersController < ApplicationController
   # before_action :require_no_user!
   def new
-    @user = User.new
+    user = User.new
     render :new
   end
 
   def create
-    @user = User.new(users_param)
-    if @user.save!
-      #login user after signing in login_user!(@user)
-      redirect_to
+    user = User.new(users_param)
+    if user.save
+      login_user!(user)
+      redirect_to user_url(user)
     else
-      flash.now[:errors] = @user.errors.full_messages
+      flash.now[:errors] = user.errors.full_messages
       render :new
     end
   end
 
+  def show
+    render json: current_user.email
+  end
+
   private
     def users_param
-      params.require(:users).permit(:email, :password)
+      params.require(:user).permit(:email, :password)
     end
 end
